8d98105f4ada8dbcfea0c9b3829ede7dbe60f9a8
Bartosz Mackiewicz <simson.kenny@gmail.com>
2017-03-31 00:22:22 +0200

    Authorization added, TODO: Finish the seedRoleUser migration

--- GameCraft/App_Start/FilterConfig.cs
+++ GameCraft/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace GameCraft
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- GameCraft/Controllers/AccountController.cs
+++ GameCraft/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using GameCraft.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GameCraft.Controllers
 {
@@ -155,6 +156,7 @@ namespace GameCraft.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
@@ -167,7 +169,6 @@ namespace GameCraft.Controllers
                 }
                 AddErrors(result);
             }
-
             // If we got this far, something failed, redisplay form
             return View(model);
         }
